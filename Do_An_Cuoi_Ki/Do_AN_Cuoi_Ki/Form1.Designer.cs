ccb2a4d6c82f05363ca6be61558d61c756a62282
bibichanel <lytrung15901@gmail.com>
2020-12-21 06:42:27 +0700

    Update

--- Do_An_Cuoi_Ki/Do_AN_Cuoi_Ki/Form1.Designer.cs
+++ Do_An_Cuoi_Ki/Do_AN_Cuoi_Ki/Form1.Designer.cs
@@ -28,13 +28,301 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnDetectFace = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtNamePerson = new System.Windows.Forms.TextBox();
+            this.picDetect = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listViewDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listViewQRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetect)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picCapture
+            // 
+            this.picCapture.Location = new System.Drawing.Point(12, 28);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(855, 544);
+            this.picCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picCapture.TabIndex = 0;
+            this.picCapture.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(899, 86);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(119, 23);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnDetectFace
+            // 
+            this.btnDetectFace.Location = new System.Drawing.Point(899, 128);
+            this.btnDetectFace.Name = "btnDetectFace";
+            this.btnDetectFace.Size = new System.Drawing.Size(120, 23);
+            this.btnDetectFace.TabIndex = 2;
+            this.btnDetectFace.Text = "Detect Face";
+            this.btnDetectFace.UseVisualStyleBackColor = true;
+            this.btnDetectFace.Click += new System.EventHandler(this.btnDetectFace_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(79, 212);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnAddPerson_Click);
+            // 
+            // txtNamePerson
+            // 
+            this.txtNamePerson.Location = new System.Drawing.Point(96, 109);
+            this.txtNamePerson.Name = "txtNamePerson";
+            this.txtNamePerson.Size = new System.Drawing.Size(152, 22);
+            this.txtNamePerson.TabIndex = 4;
+            // 
+            // picDetect
+            // 
+            this.picDetect.Location = new System.Drawing.Point(888, 176);
+            this.picDetect.Name = "picDetect";
+            this.picDetect.Size = new System.Drawing.Size(148, 142);
+            this.picDetect.TabIndex = 6;
+            this.picDetect.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtNamePerson);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(1042, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 558);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information Peson";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 260);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(283, 10);
+            this.panel1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "QR CODE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 345);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 160);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(150, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "MSSV:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Date of birth:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(903, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Take a Photo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1337, 28);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listViewDataToolStripMenuItem,
+            this.listViewQRToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.viewToolStripMenuItem.Text = "view";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // listViewDataToolStripMenuItem
+            // 
+            this.listViewDataToolStripMenuItem.Name = "listViewDataToolStripMenuItem";
+            this.listViewDataToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.listViewDataToolStripMenuItem.Text = "List view data";
+            // 
+            // listViewQRToolStripMenuItem
+            // 
+            this.listViewQRToolStripMenuItem.Name = "listViewQRToolStripMenuItem";
+            this.listViewQRToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.listViewQRToolStripMenuItem.Text = "List view QR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1337, 586);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picDetect);
+            this.Controls.Add(this.btnDetectFace);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.picCapture);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetect)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnDetectFace;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtNamePerson;
+        private System.Windows.Forms.PictureBox picDetect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listViewDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listViewQRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
--- Do_An_Cuoi_Ki/Do_AN_Cuoi_Ki/Form1.cs
+++ Do_An_Cuoi_Ki/Do_AN_Cuoi_Ki/Form1.cs
@@ -7,14 +7,133 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Do_AN_Cuoi_Ki
 {
     public partial class Form1 : Form
     {
+        //-----------Biến---------------
+        private Capture videoCapture = null;
+        private Image<Bgr, Byte> currentFrame = null;
+        Mat frame = new Mat();
+        private bool faceDetectionEnable = false;
+
+        CascadeClassifier faceCascade = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+
+        List<Image<Gray, Byte>> TrainedFaces = new List<Image<Gray, byte>>();
+        List<int> PersonsLables = new List<int>();
+        bool EnableSaveImage = false;
+        List<string> PersonsNames = new List<string>();
+        //---------------------
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            videoCapture = new Capture();
+            videoCapture.ImageGrabbed += ProcessFrame;
+            videoCapture.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            // Bước 1: bắt hình ảnh.
+            if (videoCapture != null && videoCapture.Ptr != IntPtr.Zero)
+            {
+                videoCapture.Retrieve(frame, 0);
+                currentFrame = frame.ToImage<Bgr, Byte>().Resize(picCapture.Width, picCapture.Height, Inter.Cubic);
+                //------------------
+
+                //Bước 2: Nhận diện khuôn mặt.
+                if (faceDetectionEnable)
+                {
+                    Mat grayImage = new Mat();
+                    CvInvoke.CvtColor(currentFrame, grayImage, ColorConversion.Bgr2Gray);
+                    CvInvoke.EqualizeHist(grayImage, grayImage);
+
+                    Rectangle[] faces = faceCascade.DetectMultiScale(grayImage, 1.1, 3, Size.Empty, Size.Empty);
+
+                    // Nếu face đã được detect 
+                    if (faces.Length > 0)
+                    {
+                        // vẽ rectangle xung quanh mặt.
+                        foreach (var face in faces)
+                        {
+                            CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Red).MCvScalar, 2);
+
+                            //Bước 3: Add Person
+                            Image<Bgr, Byte> resultImage = currentFrame.Convert<Bgr, Byte>();
+                            resultImage.ROI = face;
+                            picDetect.SizeMode = PictureBoxSizeMode.StretchImage;
+                            picDetect.Image = resultImage.Bitmap;
+
+                            if (EnableSaveImage)
+                            {
+                                string path = Directory.GetCurrentDirectory() + "\\DetectImages";
+                                if (!Directory.Exists(path))
+                                    Directory.CreateDirectory(path);
+
+                                Task.Factory.StartNew(() => {
+                                    for (int i = 0; i < 10; i++)
+                                    {
+                                        
+                                        resultImage.Resize(200, 200, Inter.Cubic).Save(path + @"\" + txtNamePerson.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy-hh-mm-ss") + ".jpg");
+                                        Thread.Sleep(1000);
+                                    }
+                                });
+
+                            }
+
+                            EnableSaveImage = false;
+                            if (btnSave.InvokeRequired)
+                            {
+                                btnSave.Invoke(new ThreadStart(delegate { btnSave.Enabled = true; }));
+                            }
+                            //------------------
+
+                        }
+                    }
+                }
+                //------------------
+
+                //Dán ảnh lên picture box.
+                picCapture.Image = currentFrame.Bitmap;
+                //------------------
+            }
+        }
+
+        private void btnDetectFace_Click(object sender, EventArgs e)
+        {
+            faceDetectionEnable = true;
+        }
+
+        private void btnAddPerson_Click(object sender, EventArgs e)
+        {
+           //btnSave.Enabled = true;
+            btnSave.Enabled = false;
+            EnableSaveImage = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            btnSave.Enabled = false;
+            btnSave.Enabled = true;
+            EnableSaveImage = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
